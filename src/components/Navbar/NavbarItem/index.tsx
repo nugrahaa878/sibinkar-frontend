2607a8c6a8596a734dd540744c3130202ab95fed
Yudha Haris Purnama <yudha@clade.ventures>
2024-03-09 18:27:04 +0700

    refactor: global navigation item

---
+++ src/components/Navbar/NavbarItem/index.tsx
@@ -0,0 +1,16 @@
+import { NavbarPageEnum } from ".."
+
+interface Props {
+    type: NavbarPageEnum,
+    page: NavbarPageEnum,
+}
+
+const NavbarItem = ({type, page} : Props) => {
+    const isSelected = type === page;
+
+    return (
+        <button className={`text-white font-semibold text-xl py-1 mx-4 ${isSelected && 'border-b-4'}`}>{Object.values(type)}</button>
+    )
+}
+
+export default NavbarItem
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,12 @@
 import policeYellow from "@/assets/police-1.png";
 import policeWhite from "@/assets/police-2.png";
 import { Button } from "../ui/button";
+import NavbarItem from "./NavbarItem";
 
 enum NavbarPageEnum {
-    personnelDatabase,
-    staffingStatus,
-    organizationalStructure,
+    personnelDatabase = "Personnel Database",
+    staffingStatus = "Staffing Status",
+    organizationalStructure = "Organizational Structure",
 }
 
 interface Props {
@@ -13,14 +14,14 @@ interface Props {
 }
 
 const Navbar = ({ page }: Props) => {
+    const pages = Object.values(NavbarPageEnum)
+    
     return (
     <div className="flex w-full bg-darkBlue items-center px-24 py-4 gap-8">
         <img src={policeYellow} alt="police-1" className="w-14" />
         <img src={policeWhite} alt="police-2" className="w-14" />
         <div className="flex mx-auto">
-            <h1 className={`text-white font-semibold text-xl py-1 mx-4 ${page === NavbarPageEnum.personnelDatabase ? 'border-b-4' : ''}`}>Personnel Database</h1>
-            <h1 className={`text-white font-semibold text-xl py-1 mx-4 ${page === NavbarPageEnum.staffingStatus ? 'border-b-4' : ''}`}>Staffing Status</h1>
-            <h1 className={`text-white font-semibold text-xl py-1 mx-4 ${page === NavbarPageEnum.organizationalStructure ? 'border-b-4' : ''}`}>Organizational Structure</h1>
+            {pages.map((item) => <NavbarItem page={page} type={item}/>)}
         </div>
         <Button className="bg-white text-black font-bold">Log Out</Button>
       </div>
