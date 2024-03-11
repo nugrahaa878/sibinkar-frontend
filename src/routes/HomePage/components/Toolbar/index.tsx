21a57e463dfbe3905ba61d63b69f21bdb01b324f
Yudha Haris Purnama <yudha@clade.ventures>
2024-03-11 18:30:30 +0700

    feat(component): add dialog

--- src/routes/HomePage/components/Toolbar/index.tsx
+++ src/routes/HomePage/components/Toolbar/index.tsx
@@ -1,15 +1,12 @@
 import { Button } from "@/components/ui/button";
 import FilterDropdown from "../FilterDropdown";
-import { DownloadIcon, Edit2 } from "lucide-react";
+import { DownloadIcon } from "lucide-react";
 
 const Toolbar = () => {
   return (
     <div className="flex w-full justify-between">
       <FilterDropdown />
       <div>
-        <Button variant="outline" className="mr-6">
-          <Edit2 className="mr-2" /> Edit Personil
-        </Button>
         <Button className="px-8">
           <DownloadIcon className="mr-2" /> Unduh
         </Button>
