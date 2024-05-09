f217cbd397548057d9a876b3be43dce4ff87fba6
Yudha Haris Purnama <yudha@clade.ventures>
2024-05-09 20:00:16 +0700

    feat: implement child offset

--- src/routes/OrganizationalStructurePage/components/OrganizationChart/Item/index.tsx
+++ src/routes/OrganizationalStructurePage/components/OrganizationChart/Item/index.tsx
@@ -1,27 +1,56 @@
+interface Node {
+  id: number;
+  position: string;
+  name: string;
+}
+
 interface Props {
   id: number;
   position: string;
   name: string;
   offset?: number;
+  childOffset: Node[];
 }
 
-const Item = ({ id, position, name, offset }: Props) => {
+const Item = ({ id, position, name, offset, childOffset }: Props) => {
   if (offset) {
     return (
-      <div className="flex items-center justify-center z-0">
-        <div className="h-[70px] w-[1px] bg-black" />
-        <div className="h-[1px] w-[100px] bg-black" />
-        <div className="w-[200px] h-[70px] p-3 bg-red-500 rounded-lg items-center mr-[-300px]">
-          <div className="text-center text-white text-sm font-semibold">
-            {position}
-          </div>
-          <div className="text-center text-white text-xs font-normal">
-            {name}
+      <div>
+        <div className="flex items-center justify-center">
+          <div className="h-[70px] w-[1px] bg-black" />
+          <div className="h-[1px] w-[100px] bg-black" />
+          <div className="w-[200px] h-[70px] p-3 bg-red-500 rounded-lg items-center mr-[-300px]">
+            <div className="text-center text-white text-sm font-semibold">
+              {position}
+            </div>
+            <div className="text-center text-white text-xs font-normal">
+              {name}
+            </div>
           </div>
         </div>
+
+        {childOffset.map((item) => {
+          return (
+            <div className="flex items-center justify-center">
+              <div className="h-[90px] w-[1px] bg-black" />
+              <div className="flex flex-col  items-center mr-[-300px] ml-[100px]">
+                <div className="h-[20px] w-[1px] bg-red-500" />
+                <div className="w-[200px] h-[70px] p-3 bg-green-500 rounded-lg">
+                  <div className="text-center text-white text-sm font-semibold">
+                    {item.position}
+                  </div>
+                  <div className="text-center text-white text-xs font-normal">
+                    {item.name}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
+
   return (
     <div className="w-[200px] h-[70px] bg-slate-500 rounded-lg flex-col justify-center items-center inline-flex">
       <div className="text-center text-white text-sm font-semibold">
--- src/routes/OrganizationalStructurePage/components/OrganizationChart/index.tsx
+++ src/routes/OrganizationalStructurePage/components/OrganizationChart/index.tsx
@@ -15,12 +15,37 @@ const OrganizationChart = () => {
         </div>
       }
     >
-      <TreeNode label={<Item id={4} position="BAMIN" name="AGUS" offset={1} />}>
-        <TreeNode label={<Item id={1} position="PAMIN TU" name="AKBP TU" />}>
-          <TreeNode label={<Item id={2} position="BAMIN" name="AGUS" />} />
+      <TreeNode
+        label={
+          <Item
+            id={4}
+            position="BAMIN"
+            name="AGUS"
+            offset={1}
+            childOffset={[]}
+          />
+        }
+      >
+        <TreeNode
+          label={
+            <Item id={1} position="PAMIN TU" name="AKBP TU" childOffset={[]} />
+          }
+        >
+          <TreeNode
+            label={
+              <Item id={2} position="BAMIN" name="AGUS" childOffset={[]} />
+            }
+          />
         </TreeNode>
         <TreeNode
-          label={<Item id={3} position="PAMIN URDAL" name="AKBP URDAL" />}
+          label={
+            <Item
+              id={3}
+              position="PAMIN URDAL"
+              name="AKBP URDAL"
+              childOffset={[]}
+            />
+          }
         >
           <TreeNode
             label={
@@ -29,23 +54,62 @@ const OrganizationChart = () => {
                 position="PAMIN URDAL"
                 name="AKBP URDAL"
                 offset={1}
+                childOffset={[]}
               />
             }
           >
-            <TreeNode label={<Item id={4} position="BAMIN" name="AGUS" />} />
-            <TreeNode label={<Item id={4} position="BAMIN" name="AGUS" />} />
+            <TreeNode
+              label={
+                <Item id={4} position="BAMIN" name="AGUS" childOffset={[]} />
+              }
+            />
+            <TreeNode
+              label={
+                <Item id={4} position="BAMIN" name="AGUS" childOffset={[]} />
+              }
+            />
           </TreeNode>
         </TreeNode>
         <TreeNode
-          label={<Item id={5} position="PAMIN YANPIM" name="AKBP YANI" />}
+          label={
+            <Item
+              id={5}
+              position="PAMIN YANPIM"
+              name="AKBP YANI"
+              childOffset={[]}
+            />
+          }
         >
           <TreeNode
-            label={<Item id={6} position="BAMIN" name="AGUS" offset={1} />}
+            label={
+              <Item
+                id={6}
+                position="BAMIN"
+                name="BAGUS URIP"
+                offset={1}
+                childOffset={[
+                  { id: 6, position: "BAMIN", name: "BAGUS URIP" },
+                  { id: 6, position: "BAMIN", name: "BAGUS PURBA" },
+                  { id: 6, position: "BAMIN", name: "BAGUS SUTISNA" },
+                ]}
+              />
+            }
           >
             <TreeNode
-              label={<Item id={5} position="PAMIN YANPIM" name="AKBP YANI" />}
+              label={
+                <Item
+                  id={5}
+                  position="PAMIN YANPIM"
+                  name="AKBP YANI"
+                  childOffset={[]}
+                />
+              }
             >
-              <TreeNode label={<Item id={6} position="BAMIN" name="AGUS" />} />
+              <TreeNode
+                label={
+                  <Item id={6} position="BAMIN" name="AGUS" childOffset={[]} />
+                }
+              />
             </TreeNode>
           </TreeNode>
         </TreeNode>
