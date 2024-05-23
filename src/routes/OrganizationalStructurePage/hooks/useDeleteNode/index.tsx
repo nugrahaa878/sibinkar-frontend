c3c5fbf79754011a0312b3cdbdbb41ac0f9005a0
Yudha Haris <yudha.haris@ui.ac.id>
2024-05-23 23:44:14 +0700

    feat: implement delete node

--- src/routes/OrganizationalStructurePage/hooks/useDeleteNode/index.tsx
+++ src/routes/OrganizationalStructurePage/hooks/useDeleteNode/index.tsx
@@ -1,11 +1,18 @@
 import axiosClient from "@/networks/apiClient";
 
 interface Props {
-  id: string;
+  chartId: string;
+  nodeId: number;
 }
 
-const useDeleteNode = async ({ id }: Props) => {
-  const response = await axiosClient.delete(`/organizational-structure/chart/${id}/`);
+const useDeleteNode = async ({ chartId, nodeId }: Props) => {
+  const data = {
+    node_id: nodeId,
+  };
+  const response = await axiosClient.delete(
+    `/organizational-structure/nodes/${chartId}/`,
+    { data }
+  );
   return response.data.success;
 };
 
